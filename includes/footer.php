b02f2d679f9a234213ec71f4b5b1587b1e851f6d
root <root@hostname.2k8r21b.com>
2013-09-07 12:59:06 -0700

    base web

---
+++ includes/footer.php
@@ -0,0 +1,15 @@
+<?php
+?>
+    </div>
+
+    <div id="footer">
+      <div class="container">
+        <p class="text-muted credit">Example courtesy <a href="http://martinbean.co.uk">Martin Bean</a> and <a href="http://ryanfait.com/sticky-footer/">Ryan Fait</a>.</p>
+      </div>
+    </div>
+
+  <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+  <script type="text/javascript" src="../bootstrap/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="../js/angular.min.js"></script>
+</body>
+</html>
